38b79293a64463c5c75b5cf1b3dc3472aa8d600c
Jesus Ch <jesuschuye28@gmail.com>
2022-09-12 02:16:24 -0500

    Agregado menu principal de javascript mas info de funciones

---
+++ items/javascript/desarrollo/funciones/funciones.js
@@ -0,0 +1,35 @@
+/* Función declarada */
+function saludar(){
+    document.write("Hola, mi nombre es Jesús");
+}
+
+//invocación
+saludar();
+
+/* Función expresada */
+const saludar2 = function(){
+    console.log("Hola, mi nombre es Jesús");
+}
+
+//invocación
+saludar2();
+
+/* Funciones flecha */
+const saludo = () => {
+    console.log("Hola me llamo Jesús");
+}
+
+//invocación
+saludo();
+
+/* Función que convierte en mayúscula */
+const mayuscula = (nombre) =>{
+    nombre = nombre.toUpperCase();
+    return nombre;
+}
+
+console.log(mayuscula("jesus"));
+
+/*Forma abreviada */
+const mayusc = nombre => nombre.toUpperCase();
+console.log(mayusc("Jesus H."));
