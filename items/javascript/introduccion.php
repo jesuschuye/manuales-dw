4119350b9e8cbb600ebe76d8c5862ee73de42030
Jesus Ch <jesuschuye28@gmail.com>
2022-09-17 02:22:21 -0500

    agregado tipos de datos en javascript en introduccion.php

--- items/javascript/introduccion.php
+++ items/javascript/introduccion.php
@@ -205,7 +205,7 @@
         </ul>
       </div>
     </article>
-    <!-- la inutabilidad -->
+    <!-- la inmutabilidad -->
     <article>
       <h3>La inmutabilidad</h3>      
       <div class="row mt-3">
@@ -220,6 +220,7 @@
         <p>Ver el artículo completo de freecodecamp <a href="./src/inmutabilidad-freecodecamp/espanol/news/que-es-inmutabilidad-en-javascript/index.html" target="_blank">aquí</a></p>        
       </div>
     </article>
+    <!-- Declaracion con const -->
     <article>
       <h3>Declaración con <strong>const</strong></h3>
       <div class="row mt-3">
@@ -270,6 +271,128 @@
         </pre>
       </div>
     </article>
+    <!-- Tipos de datos -->
+    <article>
+      <h3>Tipos de datos en Javascript</h3>
+      <div class="row mt-3">
+        <p class="destacado">Existen 2 tipos de datos en Javascript: <strong>Primitivos</strong> y <strong>Estructurales</strong>.</p>
+        <ul class="listado">
+          <li><i class="bi bi-back"></i>Los datos primitivos son controlados por el operador <strong><em>typeof</em></strong>.</li>
+          <li><i class="bi bi-back"></i>Los datos estructurales son controlados por el operador <strong><em>instanceof</em></strong></li>
+        </ul>        
+        <h4>Tipos de Datos Primitivos</h4>
+        <dl>
+          <dt><b><em>typeof</em></b></dt>
+          <dd>Devuelve el tipo de dato de una variable</dd>
+        </dl>
+        <div class="div-table">
+          <table class="table table-striped-columns">
+            <thead>
+              <tr>              
+                <th class="text-center">Nombre</th>
+                <th class="text-center">Valores</th>
+                <th class="text-center">typeof ===</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>              
+                <td>Undefined</td>
+                <td>Existe, pero no tiene valor</td>
+                <td>'undefined'</td>
+                
+              </tr>
+              <tr>              
+                <td>Boolean</td>
+                <td>true, false</td>                
+                <td>'boolean'</td>
+              </tr>
+              <tr>              
+                <td>Number</td>
+                <td>Number, BigItn</td>
+                <td>'number'</td>
+              </tr>
+              <tr>              
+                <td>String</td>
+                <td>valores numéricos (indice) que indican la posicion (comienza en 0)<br /> del string dentro de la cadena de caracteres.</td>
+                <td>'string'</td>
+              </tr>
+              <tr>              
+                <td>Symbol</td>
+                <td></td>
+                <td>'symbol'</td>
+              </tr>
+              <tr>
+                <td>Null</td>
+                <td>vacío</td>
+                <td>'null'</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <h4>Tipos de Datos Estructurales</h4>
+        <dl>
+          <dt><b><em>instanceof</em></b></dt>
+          <dd>verifica si un objeto en su cadena de prototipos contiene la propiedad <strong><em>prototype</em></strong> de un constructor</dd>
+        </dl>
+        <div class="div-table">
+          <table class="table table-striped-columns">
+            <thead>
+              <tr>              
+                <th class="text-center">Nombre</th>
+                <th class="text-center">Tipo de Objeto</th>
+                <th class="text-center">instanceof ===</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Function</td>
+                <td>const funcion = new Function</td>
+                <td>funcion instanceof Function <span class="comentario">//true</span></td>
+                
+              </tr>
+              <tr>
+                <td>Object</td>
+                <td>const obj = new Object</td>                
+                <td>obj instanceof Object  <span class="comentario">//true</span></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>const arr = new Array</td>
+                <td>arr instanceof Array  <span class="comentario">//true</span></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>const map = new Map</td>
+                <td>map instanceof Map  <span class="comentario">//true</span></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>const set = new Set</td>
+                <td>set instanceof Set  <span class="comentario">//true</span></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>const fecha = new Date</td>
+                <td>fecha instanceof Date  <span class="comentario">//true</span></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>const weakmap = new WeakMap</td>
+                <td>weakmap instanceof WeakMap  <span class="comentario">//true</span></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>const weakset = new WeakSet</td>
+                <td>weakset instanceof WeakSet  <span class="comentario">//true</span></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <pre class="pre-content-list">    
+
+        </pre>
+      </div>
+    </article>
   </div>
 </section>
 
@@ -306,4 +429,12 @@
       <span class="text-info">&lt;/head&gt;</span>
     </pre>
   </div>
+ -->
+
+<!-- 
+<dl>
+  <dt><b><em>defer</em></b></dt>
+  <dd>Difiere la carga del documento permitiendo esperar a que se cargue todo el <b><em>DOM</em></b> del  documento html.
+  </dd>
+</dl>
  -->
